5c29efcb608db6a3a90b6b5d70d2d835b51d4e4f
amritjangid <amrit.jangid@goibibo.com>
2016-12-08 14:59:35 +0530

    added spark-shell support

--- src/main/scala/com/goibibo/dp/mShift/CoreHandler.scala
+++ src/main/scala/com/goibibo/dp/mShift/CoreHandler.scala
@@ -5,32 +5,34 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import org.slf4j.{Logger, LoggerFactory}
 
-
 object coreHandler {
     private val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
-    def init(cargs:Seq[String], appName: String) = {
+    def initCore(sqlContext:SQLContext, cargs:Seq[String]) = {
+        val sc = sqlContext.sparkContext
 
-        var configFileName = if(cargs.length > 0) cargs(0) else { 
-            throw new IllegalArgumentException(s"Pass configuration file as the first argument") 
+        var configFileName = if(cargs.length > 0) cargs(0) else {
+            throw new IllegalArgumentException(s"Pass configuration file as the first argument")
         }
         logger.info("configFileName  = {}", configFileName)
 
-        val dataMapping = SchemaReader.readMapping(configFileName) 
+        val dataMapping = SchemaReader.readMapping(configFileName)
         logger.info("dataMapping  = {}", dataMapping)
-        
-        val conf        = new SparkConf().setAppName(appName)
-        val sc          = new SparkContext(conf)
+
         System.setProperty("fs.s3a.endpoint", "s3.ap-south-1.amazonaws.com");
         System.setProperty("com.amazonaws.services.s3.enableV4", "true");
         sc.hadoopConfiguration.set("fs.s3a.endpoint","s3.ap-south-1.amazonaws.com")
-        val sqlContext  = new org.apache.spark.sql.SQLContext(sc)
-        logger.info("sqlContext created")
 
         val mongoRDD   = MongoDataImporter.loadData(sc, dataMapping)
         (mongoRDD, dataMapping, sqlContext)
     }
 
+    def init(cargs:Seq[String], appName: String) = {
+         val conf        = new SparkConf().setAppName(appName)
+         val sc          = new SparkContext(conf)
+         val sqlContext  = new org.apache.spark.sql.SQLContext(sc)
+         initCore(sqlContext, cargs)
+    }
 
     def redshiftLoad(dataMapping:DataMapping, rdd:RDD[Row], sqlContext:SQLContext ):Unit = {
 
@@ -48,9 +50,24 @@ object coreHandler {
             case Some(fm) => mongoRDD.flatMap( fm )
             case None       => mongoRDD
         }
-        
+
         redshiftLoad( dataMapping, transformedMongoRDD, sqlContext )
-        
-    }    
+
+    }
+
+
+    def runFromShell(sqlC:SQLContext, cargs:Seq[String], customFlatMap: Option[Row => Seq[Row] ] ) = {
+        val (mongoRDD, dataMapping, sqlContext) = initCore(sqlC, cargs)
+
+        val transformedMongoRDD = customFlatMap match {
+            case Some(fm) => mongoRDD.flatMap( fm )
+            case None       => mongoRDD
+        }
+
+        redshiftLoad( dataMapping, transformedMongoRDD, sqlContext )
+
+    }
 }
+
+
 
